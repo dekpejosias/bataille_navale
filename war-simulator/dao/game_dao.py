db617269d6aecb28d25b0517207d133e0eae92f3
dekpejosias <dekpe.deffon@centrale-casablanca.ma>
2022-12-28 20:19:13 +0100

    Correction d'indentation pour les fonctions map_to..

--- war-simulator/dao/game_dao.py
+++ war-simulator/dao/game_dao.py
@@ -76,14 +76,14 @@ class GameDao:
         self.db_session.commit()
         return game_entity.id
 
-    def create_vessel(self, vessel: Vessel) -> int:
-        vessel_entity = map_to_vessel_entity(vessel)
+    def create_vessel(self, battlefield_id: int, vessel: Vessel) -> int:
+        vessel_entity = map_to_vessel_entity(battlefield_id, vessel)
         self.db_session.add(vessel_entity)
         self.db_session.commit()
         return vessel_entity.id
 
-    def create_player(self, player:Player) -> int:
-        player_entity = map_to_player_entity(player)
+    def create_player(self, player: Player, game_id) -> int:
+        player_entity = map_to_player_entity(player, game_id)
         self.db_session.add(player_entity)
         self.db_session.commit()
         return player_entity.id
@@ -104,108 +104,106 @@ class GameDao:
         return map_to_player(player_entity)
 
 
-    def map_to_game_entity(game: Game) -> GameEntity:
-        game_entity = GameEntity()
-        if game.get_id() is not None:
-            game_entity.id = game.get_id()
-        for player in game.get_players():
-            player_entity = PlayerEntity()
-            player_entity.id = player.id
-            player_entity.name = player.get_name()
-            battlefield_entity = map_to_battlefield_entity(
-                player.get_battlefield())
-            vessel_entities = \
-                map_to_vessel_entities(player.get_battlefield().id,
-                                       player.get_battlefield().vessels)
-            battlefield_entity.vessels = vessel_entities
-            player_entity.battle_field = battlefield_entity
-            game_entity.players.append(player_entity)
-        return game_entity
-
-    def map_to_vessel_entities(battlefield_id: int, vessels: list[Vessel]) \
-            -> list[VesselEntity]:
-        vessel_entities: list[VesselEntity] = []
-        for vessel in vessels:
-            vessel_entity = map_to_vessel_entity(battlefield_id, vessel)
-            vessel_entities.append(vessel_entity)
-
-        return vessel_entities
-
-    def map_to_vessel_entity(battlefield_id: int, vessel: Vessel) -> VesselEntity:
-        vessel_entity = VesselEntity()
-        weapon_entity = WeaponEntity()
-        weapon_entity.id = vessel.weapon.id
-        weapon_entity.ammunitions = vessel.weapon.ammunitions
-        weapon_entity.range = vessel.weapon.range
-        weapon_entity.type = type(vessel.weapon).__name__
-        vessel_entity.id\
-            = vessel.id
-        vessel_entity.weapon = weapon_entity
-        vessel_entity.type = type(vessel).__name__
-        vessel_entity.hits_to_be_destroyed = vessel.hits_to_be_destroyed
-        vessel_entity.coord_x = vessel.coordinates[0]
-        vessel_entity.coord_y = vessel.coordinates[1]
-        vessel_entity.coord_z = vessel.coordinates[2]
-        vessel_entity.battle_field_id = battlefield_id
-        return vessel_entity
-
-    def map_to_player_entity(player: Player) -> PlayerEntity:
+def map_to_game_entity(game: Game) -> GameEntity:
+    game_entity = GameEntity()
+    if game.get_id() is not None:
+        game_entity.id = game.get_id()
+    for player in game.get_players():
         player_entity = PlayerEntity()
         player_entity.id = player.id
-        player_entity.name = player.name
-        player_entity.battle_field = map_to_battlefield_entity(
+        player_entity.name = player.get_name()
+        battlefield_entity = map_to_battlefield_entity(
             player.get_battlefield())
-        return player_entity
+        vessel_entities = \
+            map_to_vessel_entities(player.get_battlefield().id,
+                                   player.get_battlefield().vessels)
+        battlefield_entity.vessels = vessel_entities
+        player_entity.battle_field = battlefield_entity
+        game_entity.players.append(player_entity)
+    return game_entity
 
-    def map_to_battlefield_entity(battlefield: Battlefield) -> BattlefieldEntity:
-        battlefield_entity = BattlefieldEntity()
-        battlefield_entity.id = battlefield.id
-        battlefield_entity.max_x = battlefield.max_x
-        battlefield_entity.max_y = battlefield.max_y
-        battlefield_entity.max_z = battlefield.max_z
-        battlefield_entity.min_x = battlefield.min_x
-        battlefield_entity.min_y = battlefield.min_y
-        battlefield_entity.min_z = battlefield.min_z
-        battlefield_entity.max_power = battlefield.max_power
-        return battlefield_entity
+def map_to_vessel_entities(battlefield_id: int, vessels: list[Vessel]) -> list[VesselEntity]:
+    vessel_entities: list[VesselEntity] = []
+    for vessel in vessels:
+        vessel_entity = map_to_vessel_entity(battlefield_id, vessel)
+        vessel_entities.append(vessel_entity)
 
-    def map_to_battlefield(battlefield_entity: BattlefieldEntity) -> Battlefield:
-        min_x = battlefield_entity.min_x
-        max_x = battlefield_entity.max_x
-        min_y = battlefield_entity.min_y
-        max_y = battlefield_entity.max_y
-        min_z = battlefield_entity.min_z
-        max_z = battlefield_entity.max_z
-        max_power = battlefield_entity.max_power
-        battlefield = Battlefield(min_x, max_x, min_y, max_y, min_z, max_z, max_power)
-        return battlefield
+    return vessel_entities
 
-    def map_to_player(player_entity) -> Player:
-        name = player_entity.name
-        battle_field = map_to_battlefield(player_entity.battle_field)
-        player = Player(name, battle_field)
-        player.id = player_entity.id
-        return player
+def map_to_vessel_entity(battlefield_id: int, vessel: Vessel) -> VesselEntity:
+    vessel_entity = VesselEntity()
+    weapon_entity = WeaponEntity()
+    weapon_entity.id = vessel.weapon.id
+    weapon_entity.ammunitions = vessel.weapon.ammunitions
+    weapon_entity.range = vessel.weapon.range
+    weapon_entity.type = type(vessel.weapon).__name__
+    vessel_entity.id = vessel.id
+    vessel_entity.weapon = weapon_entity
+    vessel_entity.type = type(vessel).__name__
+    vessel_entity.hits_to_be_destroyed = vessel.hits_to_be_destroyed
+    vessel_entity.coord_x = vessel.coordinates[0]
+    vessel_entity.coord_y = vessel.coordinates[1]
+    vessel_entity.coord_z = vessel.coordinates[2]
+    vessel_entity.battle_field_id = battlefield_id
+    return vessel_entity
 
-    def map_to_game(game_entity : GameEntity)-> Game:
-        if game_entity.id is not None:
-            game = Game(game_entity.id)
-        for player_entity in game_entity.players:
-            player = map_to_player(player_entity)
-            game.add_player(player)
-        return game
+def map_to_player_entity(player: Player, game_id: int) -> PlayerEntity:
+    player_entity = PlayerEntity()
+    player_entity.id = player.id
+    player_entity.name = player.name
+    player_entity.battle_field = map_to_battlefield_entity(
+        player.get_battlefield())
+    player_entity.game_id = game_id
+    return player_entity
 
+def map_to_battlefield_entity(battlefield: Battlefield) -> BattlefieldEntity:
+    battlefield_entity = BattlefieldEntity()
+    battlefield_entity.id = battlefield.id
+    battlefield_entity.max_x = battlefield.max_x
+    battlefield_entity.max_y = battlefield.max_y
+    battlefield_entity.max_z = battlefield.max_z
+    battlefield_entity.min_x = battlefield.min_x
+    battlefield_entity.min_y = battlefield.min_y
+    battlefield_entity.min_z = battlefield.min_z
+    battlefield_entity.max_power = battlefield.max_power
+    return battlefield_entity
 
-    def map_to_vessel(vessel_entity : VesselEntity)-> Vessel:
-        x = vessel_entity.coord_x
-        y = vessel_entity.coord_y
-        z = vessel_entity.coord_z
-        hits = vessel_entity.hits_to_be_destroyed
-        ammunitions = vessel_entity.weapon.ammunitions
-        range = vessel_entity.weapon.range
-        weapon = Weapon(ammunitions,range)
-        vessel = Vessel (x,y,z,hits,weapon)
-        return vessel
+def map_to_battlefield(battlefield_entity: BattlefieldEntity) -> Battlefield:
+    min_x = battlefield_entity.min_x
+    max_x = battlefield_entity.max_x
+    min_y = battlefield_entity.min_y
+    max_y = battlefield_entity.max_y
+    min_z = battlefield_entity.min_z
+    max_z = battlefield_entity.max_z
+    max_power = battlefield_entity.max_power
+    battlefield = Battlefield(min_x, max_x, min_y, max_y, min_z, max_z, max_power)
+    return battlefield
+
+def map_to_player(player_entity) -> Player:
+    name = player_entity.name
+    battle_field = map_to_battlefield(player_entity.battle_field)
+    player = Player(name, battle_field)
+    player.id = player_entity.id
+    return player
+
+def map_to_game(game_entity: GameEntity) -> Game:
+    if game_entity.id is not None:
+        game = Game(game_entity.id)
+    for player_entity in game_entity.players:
+        player = map_to_player(player_entity)
+        game.add_player(player)
+    return game
+
+def map_to_vessel(vessel_entity: VesselEntity) -> Vessel:
+    x = vessel_entity.coord_x
+    y = vessel_entity.coord_y
+    z = vessel_entity.coord_z
+    hits = vessel_entity.hits_to_be_destroyed
+    ammunition = vessel_entity.weapon.ammunitions
+    rayon = vessel_entity.weapon.range
+    weapon = Weapon(ammunition, rayon)
+    vessel = Vessel(x, y, z, hits, weapon)
+    return vessel
 
 
 
